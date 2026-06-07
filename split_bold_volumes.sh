70fc3c23138fc502a6e8685ddb18a841de1b0af7
jshahki <166073019+jshahki@users.noreply.github.com>
2026-06-06 21:55:43 -0700

    Update split_bold_volumes.sh

--- split_bold_volumes.sh
+++ split_bold_volumes.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=30000
+#SBATCH --mem-per-cpu=60000
 #SBATCH --output=logs/split_%A_%a.out
 #SBATCH --error=logs/split_%A_%a.err
 
