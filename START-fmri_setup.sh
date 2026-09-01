c956e82cb36183274dc92b9820d0353fc85069e3
jshahki <166073019+jshahki@users.noreply.github.com>
2026-09-01 13:08:07 -0700

    Update START-fmri_setup.sh

--- START-fmri_setup.sh
+++ START-fmri_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=START-fmri_setup
-#SBATCH --time=02:00:00
+#SBATCH --time=06:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=10000
 #SBATCH --account=def-woodward
