254b05f4975648dbdffe9bcd8b4e1f43480a853f
jshahki <166073019+jshahki@users.noreply.github.com>
2026-09-01 13:08:57 -0700

    Update reslice_bold_volumes.sh

--- reslice_bold_volumes.sh
+++ reslice_bold_volumes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=reslice_bold
 #SBATCH --account=def-woodward
-#SBATCH --time=02:00:00
+#SBATCH --time=15:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=30000
 #SBATCH --output=logs/reslice_%A_%a.out
